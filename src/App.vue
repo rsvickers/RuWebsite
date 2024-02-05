116402f24a9832ff268bc9d02bc64bad78f43e4f
Rhys Vickers <141367198+rsvickers@users.noreply.github.com>
2024-02-05 13:41:49 -0700

    got rid of things

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
   <main>
     <router-view />
   </main>
-  <footer class="bg-dark text-light">
-    <!-- Made with 💖 by CodeWorks -->
-  </footer>
+  <!-- <footer class="bg-dark text-light">
+    Made with 💖 by CodeWorks
+  </footer> -->
 </template>
 
 <script>
